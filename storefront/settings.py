ce01276554ba20d46abb98dc97a64a19fd855542
Arafat Hossain <arafat6462@gmail.com>
2025-09-20 15:28:49 +0600

    add django-debug-toolbar for enhanced debugging and update settings and URLs

--- storefront/settings.py
+++ storefront/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'playground',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+
+INTERNAL_IPS = [
+    '127.0.0.1'
+]
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda request: True
+}
 
 ROOT_URLCONF = 'storefront.urls'
 
