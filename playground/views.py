07a50865aeab50433693b8eb1955ba212b5b3a92
Arafat Hossain <arafat6462@gmail.com>
2025-09-20 11:49:52 +0600

    implement hello view and add corresponding HTML template

--- playground/views.py
+++ playground/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def say_hello(request):
-    return HttpResponse("Hello Arafat!")
+    # return HttpResponse("Hello Arafat!")
+    return render(request, 'hello.html', {'name': 'Arafat'})
