0fa327ebff79c35763c20518742cf49819e5b24d
Arafat Hossain <arafat6462@gmail.com>
2025-09-21 10:22:57 +0600

    add Address model with foreign key relationship to Customer

--- store/models.py
+++ store/models.py
@@ -43,3 +43,19 @@ class Order(models.Model):
 
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+
+    # This line creates a one-to-one relationship between your model and the Customer model.
+    # Each instance of your model is linked to exactly one Customer.
+    # If the Customer is deleted, the related instance is also deleted (CASCADE).
+    # The field is also set as the primary key for this model.
+    # customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+    
+    # This line creates a many-to-one relationship (ForeignKey) to the Customer model.
+    # Multiple instances of your model can be linked to the same Customer.
+    # If the Customer is deleted, all related instances are also deleted (CASCADE).
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
