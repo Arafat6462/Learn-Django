7d067860e4507e51479f07f6c865489ef85145ae
Arafat Hossain <arafat6462@gmail.com>
2025-09-27 14:44:15 +0600

    configure Docker and PostgreSQL settings: update docker-compose.yml and switch database to PostgreSQL in settings.py

--- storefront/settings.py
+++ storefront/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_db',
+        'USER': 'postgres',
+        'PASSWORD': 'password123',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
