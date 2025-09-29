b90f00e21064ba2a6420e319034972128736d2af
Arafat Hossain <arafat6462@gmail.com>
2025-09-29 10:45:21 +0600

    refactor say_hello view: add example for retrieving a single product by primary key and enhance exception handling

--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from store.models import Product
 
@@ -42,5 +43,20 @@ def say_hello(request):
     
     for product in query_set:
         print(product)
+
+
+# Retrieving a single object by primary key (id)
+    # if you try to get a product that does not exist, it will raise an exception
+    try:
+        # Fetches a single product with primary key 1 (executes SQL immediately). pk is shorthand for primary key. in django pk automatically maps to id field
+        product = Product.objects.get(pk=1) 
+        # Both are same. id and pk are interchangeable in this context. id is the actual field name in the model, while pk is a more general term that always refers to the primary key of the model.
+        product = Product.objects.get(id=1)
+    
+    except ObjectDoesNotExist:
+        pass
+    
+    exist_bool = Product.objects.filter(id=1).exists()  # Returns True if a product with id=1 exists, otherwise False
+    product_2 = Product.objects.filter(id=0).first()  # Returns None if no match is found, avoiding exception
 
     return render(request, 'hello.html', {'name': 'Arafat'})
